04f405526d3f5407604ace24545fce935d3280a1
Iddahodhiambo <iddahawuor@yahoo.com>
2023-02-06 12:51:51 +0300

    done

--- methods.rb
+++ methods.rb
@@ -1,1 +1,34 @@
-# Your code here!
+def greet_programmer
+    puts "Hello, programmer!"
+
+end
+ 
+def greet(name)
+    puts "Hello, #{name}!" 
+end
+greet("Naureen")
+
+def greet_with_default(name = "programmer")
+    
+    puts "Hello, #{name}!" 
+end
+
+greet_with_default();
+greet_with_default("Naureen")
+
+def add(num1,num2)
+    return num1+num2;
+
+end
+add(2,5);
+
+def halve(num1)
+    if num1.class != Integer
+    return nil
+    
+    end
+
+    num1/2
+end
+
+halve(6);
